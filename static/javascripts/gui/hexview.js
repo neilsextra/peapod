39979b56012ebdf9b80dce3b3b3284e82f236f84
Dr. Neil Brittliff <Neil.Brittliff@outlook.com>
2024-03-26 05:18:10 +0000

    Added Hex View

--- static/javascripts/gui/hexview.js
+++ static/javascripts/gui/hexview.js
@@ -1,12 +1,27 @@
 function hexview(data) {
     var innerHTML = "";
+    var chars = "";
+    var i = 0;
+
     for (i in u = new Uint8Array(data)) {
-        innerHTML += (i % 16 ? ' ' : `<p></p>` + 
-            (1e7 + (+i)[t = 'toString'](16)).slice(-8) + ' | ') + 
-            (0 + u[i][t] (16)).slice(-2); 
-  
+        innerHTML += (i % 16 ? ' ' : ` ${i != 0 ? " | " + chars + "<p></p>" : ""}` +
+            (1e7 + (+i)[t = 'toString'](16)).slice(-8) + ' | ') +
+            (0 + u[i][t](16)).slice(-2);
+
+        if (!(i % 16)) {
+            chars = "";
+        }
+
+        chars += String.fromCharCode(u[i]).replace(/[^ -~]+/g, ".").replace(" ", "&nbsp;");
+
+    }
+
+    console.log(`I is ${i} - ${i % 16}`);
+
+    if (i % 16) {
+        innerHTML += " | ".padStart((16 - (i % 16)) * 3, "-").replace(/-/g, "&nbsp;") + chars;
     }
 
     return innerHTML;
-    
+
 }
